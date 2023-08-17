0eb337afc64a174182faeed7cbaa6f9a77956046
Leonardo-PC <leonardonaka75@gmail.com>
2023-08-17 13:51:44 -0300

    arrays

--- JavaScript/teste.js
+++ JavaScript/teste.js
@@ -1,5 +1,23 @@
-function number (){
-    console.log(42)
-}
+// fruits = ['apple', 'banana', 'grapes']
 
-number()
+// fruits.forEach(capitalize)
+// console.log(fruits[0]);
+// fruits.forEach(print)
+
+// function capitalize (element, index, array) {
+//     array[index] = element[0].toUpperCase() + element.substring(1)
+
+// }
+
+// function print(element){
+//     console.log(element)
+// }
+
+//
+
+let temps = [0, 10, 20, 30];
+
+let conversion = temps.map(function (temp){
+    return temp * 9/5 + 32
+})
+console.log(conversion);
