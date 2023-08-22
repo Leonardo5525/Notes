26697c2859b4e61d5ed6b4d0742506a5e505d466
Leonardo-PC <leonardonaka75@gmail.com>
2023-08-22 13:27:57 -0300

    aual05

---
+++ JavaScript/Curso_Video/Aula04/script.js
@@ -0,0 +1,3 @@
+window.alert('Minha primeira mensgaem') // pop up de mensagem
+window.confirm('Está gostando de JS?') // pop up de pegunta de sim ou cancel
+window.prompt('Qual o seu nome?') // pop up de input do que desejar
